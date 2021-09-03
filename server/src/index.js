47148b25f7cb14c7d13dda385199752ebf49b6e4
Zendy <nguyenduc.051194@gmail.com>
2021-09-03 15:46:36 +0700

    Initial project

---
+++ server/src/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+
+const app = express();
+
+app.get("/", (req, res) => res.send(`Hello World`));
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
