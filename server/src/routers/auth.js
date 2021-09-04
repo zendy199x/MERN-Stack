48080bd2ceea02e8abee5496a6a4ea9d417c7a06
Zendy <nguyenduc.051194@gmail.com>
2021-09-04 15:07:58 +0700

    server: Create API check if user logged in

--- server/src/routers/auth.js
+++ server/src/routers/auth.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const argon2 = require("argon2");
 const jwt = require("jsonwebtoken");
+const verifyToken = require("../middleware/auth");
 
 const router = express.Router();
 
 const User = require("../models/User");
+
+// @route GET api/auth
+// @desc Check if user is logged in
+// @access Public
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user)
+      return res
+        .status(400)
+        .json({ success: false, message: "User not found" });
+
+    res.json({ success: true, user });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: `Internal server error` });
+  }
+});
 
 // @route POST api/auth/register
 // @desc Register user
