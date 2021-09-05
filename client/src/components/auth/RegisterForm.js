e2dbc7c1d45c0716c57b1145a6c0d1a0fb7e3abe
Zendy <nguyenduc.051194@gmail.com>
2021-09-05 12:05:01 +0700

    client: Register user

--- client/src/components/auth/RegisterForm.js
+++ client/src/components/auth/RegisterForm.js
@@ -1,17 +1,62 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import AlertMessage from "../layout/AlertMessage";
 
 const RegisterForm = () => {
+  // Context
+  const { registerUser } = useContext(AuthContext);
+
+  const [registerForm, setRegisterForm] = useState({
+    username: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [alert, setAlert] = useState(null);
+
+  const { username, password, confirmPassword } = registerForm;
+
+  const onChangeRegisterForm = (event) =>
+    setRegisterForm({
+      ...registerForm,
+      [event.target.name]: event.target.value,
+    });
+
+  const handleRegister = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      setAlert({ type: "danger", message: "Passwords do not match" });
+      setTimeout(() => setAlert(null), 5000);
+      return;
+    }
+
+    try {
+      const registerData = await registerUser(registerForm);
+      if (!registerData.success) {
+        setAlert({ type: "danger", message: registerData.message });
+        setTimeout(() => setAlert(null), 5000);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
-      <Form className="my-4">
+      <Form className="my-4" onSubmit={handleRegister}>
+        <AlertMessage info={alert} />
+
         <Form.Group>
           <Form.Control
             type="text"
             placeholder="Username"
             name="username"
             required
+            value={username}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Form.Group className="my-2">
@@ -20,6 +65,8 @@ const RegisterForm = () => {
             placeholder="Password"
             name="password"
             required
+            value={password}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Form.Group>
@@ -28,6 +75,8 @@ const RegisterForm = () => {
             placeholder="Confirm Password"
             name="confirmPassword"
             required
+            value={confirmPassword}
+            onChange={onChangeRegisterForm}
           />
         </Form.Group>
         <Button variant="success" type="submit" className="mt-2">
--- client/src/contexts/AuthContext.js
+++ client/src/contexts/AuthContext.js
@@ -64,8 +64,27 @@ const AuthContextProvider = ({ children }) => {
     }
   };
 
+  // Register
+  const registerUser = async (userForm) => {
+    try {
+      const response = await axios.post(`${apiUrl}/auth/register`, userForm);
+      if (response.data.success)
+        localStorage.setItem(
+          LOCAL_STORAGE_TOKEN_NAME,
+          response.data.accessToken
+        );
+
+      await loadUser();
+
+      return response.data;
+    } catch (error) {
+      if (error.response.data) return error.response.data;
+      else return { success: false, message: error.message };
+    }
+  };
+
   // Context data
-  const authContextData = { loginUser, authState };
+  const authContextData = { loginUser, registerUser, authState };
 
   // Return provider
   return (
